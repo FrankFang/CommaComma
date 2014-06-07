81c7979e94fd8ed9d681d1e144cc9a1640620dfa
Frank Fang <frankfang1990@gmail.com>
2014-06-07 17:39:28 +0800

    add bracket

--- CommaComma/AppDelegate.swift
+++ CommaComma/AppDelegate.swift
@@ -12,15 +12,66 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                             
     @IBOutlet var window: NSWindow
 
+    @IBOutlet var originalText : NSTextFieldCell
+    @IBOutlet var resultText : NSTextField
 
-    func applicationDidFinishLaunching(aNotification: NSNotification?) {
-        // Insert code here to initialize your application
+    @IBOutlet var convertButton : NSButton
+    
+    var preferences  = NSUserDefaults.standardUserDefaults()
+    
+    @IBAction func convertFromFullToHalf(sender : AnyObject) {
+        var original:String = originalText.stringValue
+        
+        var result = original
+        if preferences.boolForKey("singleQuote") {
+            result = result.stringByReplacingOccurrencesOfString("‘", withString: "'")
+            result = result.stringByReplacingOccurrencesOfString("’", withString: "'")
+        }
+        if preferences.boolForKey("exclamation") {
+            result = result.stringByReplacingOccurrencesOfString("！", withString: "!")
+        }
+        if preferences.boolForKey("semicolon") {
+            result = result.stringByReplacingOccurrencesOfString("；", withString: ";")
+        }
+        if preferences.boolForKey("colon") {
+            result = result.stringByReplacingOccurrencesOfString("：", withString: ":")
+        }
+        if preferences.boolForKey("period") {
+            result = result.stringByReplacingOccurrencesOfString("。", withString: ".")
+        }
+        if preferences.boolForKey("comma") {
+            result = result.stringByReplacingOccurrencesOfString("，", withString: ",")
+        }
+        if preferences.boolForKey("doubleQuote") {
+            result = result.stringByReplacingOccurrencesOfString("“", withString: "\"")
+            result = result.stringByReplacingOccurrencesOfString("”", withString: "\"")
+        }
+        if preferences.boolForKey("question") {
+            result = result.stringByReplacingOccurrencesOfString("？", withString: "?")
+        }
+        if preferences.boolForKey("bracket") {
+            result = result.stringByReplacingOccurrencesOfString("（", withString: "(")
+            result = result.stringByReplacingOccurrencesOfString("）", withString: ")")
+        }
+        
+        resultText.stringValue = result
     }
-
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        window.title = "CommaComma"
+        originalText.stringValue = "，，，"
+        
+        var config = ["singleQuote":true,"exclamation":true,"semicolon":true,"colon":true,"period":true,"comma":true,
+            "doubleQuote": true,"question": true, "bracket":false]
+        
+        preferences.registerDefaults(config)
+        
+    }
+    
     func applicationWillTerminate(aNotification: NSNotification?) {
         // Insert code here to tear down your application
     }
-
-
+    
+    
 }
 
