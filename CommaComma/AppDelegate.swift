ac6e27e9c02d591c941ebdda05ec6478b08a1698
Frank Fang <frankfang1990@gmail.com>
2014-06-07 18:08:19 +0800

    add NSTextView

--- CommaComma/AppDelegate.swift
+++ CommaComma/AppDelegate.swift
@@ -11,55 +11,52 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
                             
     @IBOutlet var window: NSWindow
-
-    @IBOutlet var originalText : NSTextFieldCell
-    @IBOutlet var resultText : NSTextField
-
+    
     @IBOutlet var convertButton : NSButton
     
     var preferences  = NSUserDefaults.standardUserDefaults()
     
     @IBAction func convertFromFullToHalf(sender : AnyObject) {
-        var original:String = originalText.stringValue
-        
-        var result = original
-        if preferences.boolForKey("singleQuote") {
-            result = result.stringByReplacingOccurrencesOfString("‘", withString: "'")
-            result = result.stringByReplacingOccurrencesOfString("’", withString: "'")
-        }
-        if preferences.boolForKey("exclamation") {
-            result = result.stringByReplacingOccurrencesOfString("！", withString: "!")
-        }
-        if preferences.boolForKey("semicolon") {
-            result = result.stringByReplacingOccurrencesOfString("；", withString: ";")
-        }
-        if preferences.boolForKey("colon") {
-            result = result.stringByReplacingOccurrencesOfString("：", withString: ":")
-        }
-        if preferences.boolForKey("period") {
-            result = result.stringByReplacingOccurrencesOfString("。", withString: ".")
-        }
-        if preferences.boolForKey("comma") {
-            result = result.stringByReplacingOccurrencesOfString("，", withString: ",")
-        }
-        if preferences.boolForKey("doubleQuote") {
-            result = result.stringByReplacingOccurrencesOfString("“", withString: "\"")
-            result = result.stringByReplacingOccurrencesOfString("”", withString: "\"")
-        }
-        if preferences.boolForKey("question") {
-            result = result.stringByReplacingOccurrencesOfString("？", withString: "?")
-        }
-        if preferences.boolForKey("bracket") {
-            result = result.stringByReplacingOccurrencesOfString("（", withString: "(")
-            result = result.stringByReplacingOccurrencesOfString("）", withString: ")")
-        }
-        
-        resultText.stringValue = result
+//        var original:String = originalText.string
+//        
+//        var result = original
+//        if preferences.boolForKey("singleQuote") {
+//            result = result.stringByReplacingOccurrencesOfString("‘", withString: "'")
+//            result = result.stringByReplacingOccurrencesOfString("’", withString: "'")
+//        }
+//        if preferences.boolForKey("exclamation") {
+//            result = result.stringByReplacingOccurrencesOfString("！", withString: "!")
+//        }
+//        if preferences.boolForKey("semicolon") {
+//            result = result.stringByReplacingOccurrencesOfString("；", withString: ";")
+//        }
+//        if preferences.boolForKey("colon") {
+//            result = result.stringByReplacingOccurrencesOfString("：", withString: ":")
+//        }
+//        if preferences.boolForKey("period") {
+//            result = result.stringByReplacingOccurrencesOfString("。", withString: ".")
+//        }
+//        if preferences.boolForKey("comma") {
+//            result = result.stringByReplacingOccurrencesOfString("，", withString: ",")
+//        }
+//        if preferences.boolForKey("doubleQuote") {
+//            result = result.stringByReplacingOccurrencesOfString("“", withString: "\"")
+//            result = result.stringByReplacingOccurrencesOfString("”", withString: "\"")
+//        }
+//        if preferences.boolForKey("question") {
+//            result = result.stringByReplacingOccurrencesOfString("？", withString: "?")
+//        }
+//        if preferences.boolForKey("bracket") {
+//            result = result.stringByReplacingOccurrencesOfString("（", withString: "(")
+//            result = result.stringByReplacingOccurrencesOfString("）", withString: ")")
+//        }
+//        
+//        resultText.string = result
     }
     
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
         window.title = "CommaComma"
-        originalText.stringValue = "，，，"
+        
         
         var config = ["singleQuote":true,"exclamation":true,"semicolon":true,"colon":true,"period":true,"comma":true,
             "doubleQuote": true,"question": true, "bracket":false]
