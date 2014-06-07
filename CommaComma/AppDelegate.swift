fd3c8b139bf2dc49521c9d8d14129d3ebc631a12
Frank Fang <frankfang1990@gmail.com>
2014-06-07 19:58:39 +0800

    mostly works

--- CommaComma/AppDelegate.swift
+++ CommaComma/AppDelegate.swift
@@ -9,60 +9,109 @@
 import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    var preferences  = NSUserDefaults.standardUserDefaults()
                             
     @IBOutlet var window: NSWindow
     
-    @IBOutlet var convertButton : NSButton
+    @IBOutlet var upperView : NSScrollView
+    @IBOutlet var lowerView : NSScrollView
     
-    var preferences  = NSUserDefaults.standardUserDefaults()
-    
-    @IBAction func convertFromFullToHalf(sender : AnyObject) {
-//        var original:String = originalText.string
-//        
-//        var result = original
-//        if preferences.boolForKey("singleQuote") {
+    @IBAction func convertFromHalfToFull(sender : AnyObject) {
+        var original = self.getOriginalText()
+        
+        var result = original
+        if preferences.boolForKey("singleQuote") {
 //            result = result.stringByReplacingOccurrencesOfString("‘", withString: "'")
 //            result = result.stringByReplacingOccurrencesOfString("’", withString: "'")
-//        }
-//        if preferences.boolForKey("exclamation") {
-//            result = result.stringByReplacingOccurrencesOfString("！", withString: "!")
-//        }
-//        if preferences.boolForKey("semicolon") {
-//            result = result.stringByReplacingOccurrencesOfString("；", withString: ";")
-//        }
-//        if preferences.boolForKey("colon") {
-//            result = result.stringByReplacingOccurrencesOfString("：", withString: ":")
-//        }
-//        if preferences.boolForKey("period") {
-//            result = result.stringByReplacingOccurrencesOfString("。", withString: ".")
-//        }
-//        if preferences.boolForKey("comma") {
-//            result = result.stringByReplacingOccurrencesOfString("，", withString: ",")
-//        }
-//        if preferences.boolForKey("doubleQuote") {
+        }
+        if preferences.boolForKey("exclamation") {
+            result = result.stringByReplacingOccurrencesOfString("!", withString: "！")
+        }
+        if preferences.boolForKey("semicolon") {
+            result = result.stringByReplacingOccurrencesOfString(";", withString: "；")
+        }
+        if preferences.boolForKey("colon") {
+            result = result.stringByReplacingOccurrencesOfString(":", withString: "：")
+        }
+        if preferences.boolForKey("period") {
+            result = result.stringByReplacingOccurrencesOfString(".", withString: "。")
+        }
+        if preferences.boolForKey("comma") {
+            result = result.stringByReplacingOccurrencesOfString(",", withString: "，")
+        }
+        if preferences.boolForKey("doubleQuote") {
 //            result = result.stringByReplacingOccurrencesOfString("“", withString: "\"")
 //            result = result.stringByReplacingOccurrencesOfString("”", withString: "\"")
-//        }
-//        if preferences.boolForKey("question") {
-//            result = result.stringByReplacingOccurrencesOfString("？", withString: "?")
-//        }
-//        if preferences.boolForKey("bracket") {
-//            result = result.stringByReplacingOccurrencesOfString("（", withString: "(")
-//            result = result.stringByReplacingOccurrencesOfString("）", withString: ")")
-//        }
-//        
-//        resultText.string = result
+        }
+        if preferences.boolForKey("question") {
+            result = result.stringByReplacingOccurrencesOfString("?", withString: "？")
+        }
+        if preferences.boolForKey("bracket") {
+            result = result.stringByReplacingOccurrencesOfString("(", withString: "（")
+            result = result.stringByReplacingOccurrencesOfString(")", withString: "）")
+        }
+        
+        self.fillResultText(result)
     }
     
+    func getOriginalText(Void) -> String{
+        return self.upperView.contentView.documentView.string
+    }
+    
+    func fillResultText(result: String) -> Bool{
+        if let view = lowerView.contentView.documentView as? NSTextView{
+            view.string = result
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    
+    @IBAction func convertFromFullToHalf(sender : AnyObject) {
+        var original = self.getOriginalText()
+        
+        var result = original
+        if preferences.boolForKey("singleQuote") {
+            result = result.stringByReplacingOccurrencesOfString("‘", withString: "'")
+            result = result.stringByReplacingOccurrencesOfString("’", withString: "'")
+        }
+        if preferences.boolForKey("exclamation") {
+            result = result.stringByReplacingOccurrencesOfString("！", withString: "!")
+        }
+        if preferences.boolForKey("semicolon") {
+            result = result.stringByReplacingOccurrencesOfString("；", withString: ";")
+        }
+        if preferences.boolForKey("colon") {
+            result = result.stringByReplacingOccurrencesOfString("：", withString: ":")
+        }
+        if preferences.boolForKey("period") {
+            result = result.stringByReplacingOccurrencesOfString("。", withString: ".")
+        }
+        if preferences.boolForKey("comma") {
+            result = result.stringByReplacingOccurrencesOfString("，", withString: ",")
+        }
+        if preferences.boolForKey("doubleQuote") {
+            result = result.stringByReplacingOccurrencesOfString("“", withString: "\"")
+            result = result.stringByReplacingOccurrencesOfString("”", withString: "\"")
+        }
+        if preferences.boolForKey("question") {
+            result = result.stringByReplacingOccurrencesOfString("？", withString: "?")
+        }
+        if preferences.boolForKey("bracket") {
+            result = result.stringByReplacingOccurrencesOfString("（", withString: "(")
+            result = result.stringByReplacingOccurrencesOfString("）", withString: ")")
+        }
+        
+        self.fillResultText(result)
+    }
+    
+    var config = ["singleQuote":true,"exclamation":true,"semicolon":true,"colon":true,"period":true,"comma":true,
+        "doubleQuote": true,"question": true, "bracket":false]
+    
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
-        window.title = "CommaComma"
-        
-        
-        var config = ["singleQuote":true,"exclamation":true,"semicolon":true,"colon":true,"period":true,"comma":true,
-            "doubleQuote": true,"question": true, "bracket":false]
-        
         preferences.registerDefaults(config)
-        
     }
     
     func applicationWillTerminate(aNotification: NSNotification?) {
