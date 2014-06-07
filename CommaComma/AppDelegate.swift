8dfb6a1904ec388cc3ce5890c43b3d68cd6dd84b
Frank Fang <frankfang1990@gmail.com>
2014-06-07 13:43:05 +0800

    Initial Commit

---
+++ CommaComma/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CommaComma
+//
+//  Created by FrankFang on 14-6-7.
+//  Copyright (c) 2014年 FrankFang. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CommaComma/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  CommaComma
+//
+//  Created by FrankFang on 14-6-7.
+//  Copyright (c) 2014年 FrankFang. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
